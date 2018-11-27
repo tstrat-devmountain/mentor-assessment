ec8b05b459eeb3f40a2caee48e819df6b75bd102
tstrat <tstrat@email.arizona.edu>
2018-11-26 22:37:35 -0700

    Implemented Add Items to List

--- src/components/ListDisplay.js
+++ src/components/ListDisplay.js
@@ -3,34 +3,63 @@ import { connect } from 'react-redux';
 import { getList, setList } from '../redux/reducer';
 import axios from 'axios';
 
+import ListItem from './ListItem';
+
 
 class ListDisplay extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+        }
+    }
     
     componentDidMount() {
         // do axios here to initialize redux
-        this.props.setList([{"id":0,"title":"walk the dog","description":"yo dawg!","completed":true},{"id":1,"title":"yo Dawg!","description":"snoop dog","completed":true}]);
+        this.props.setList([{"id":0,"title":"walk the dog","description":"yo dawg!","completed":false},{"id":1,"title":"yo Dawg!","description":"snoop dog","completed":false}]);
     }
 
+    markComplete = (id) => {
+        console.log('update');
+        const { tasks } = this.props;
+        const newList = tasks.slice();
+        newList[newList.findIndex(task => task.id === id)].completed = true;
+        
+        this.props.setList(newList);
+    }
 
+    addTask = () => {
+        // axios
+
+        const { tasks } = this.props;
+        const newList = tasks.slice();
+        const { title } = this.state;
+        newList.push({
+            id: Math.random() * 999,
+            title,
+            description: '',
+            completed: false
+        })
+        this.props.setList(newList);
+        this.setState({ title: '' }); // reset input field
+    }
 
     render() {
         // map list to display
-        console.log('list display', this.props.tasks);
         const { tasks } = this.props;
         if (!tasks.length) {
             return <></>;
         }
         
-        const taskList = tasks.map(task => {
-            return (
-                <div>
-                    <h1>{task.title}</h1>
-                    <p>{task.description}</p>
-                </div>
-            )
-        })
+        const taskList = tasks.map(task => <ListItem key={task.id} task={task} completeFn={this.markComplete}/>);
+            
         return (
             <div>
+                <div>
+                    <h1>Add New Task:</h1>
+                    <input value={this.state.title} onChange={e => this.setState({ title: e.target.value })} />
+                    <button onClick={this.addTask}>Add Task</button>
+                </div>
                 {taskList}
             </div>
     )}
---
+++ src/components/ListItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { strikethrough } from 'ansi-colors';
+
+const ListItem = (props) => {
+    const { task } = props;
+    const c = task.completed 
+        ? 
+        { 
+            textDecoration: 'line-through',
+            cursor: 'no-drop'
+        } 
+        : { }
+
+    console.log(task.completed);
+    return (
+        <div className="task" style={task.completed ? {textDecoration: 'line-through'} : {}}>
+            <h1>{task.title}</h1>
+            <p>{task.description}</p>
+            <button style={c} onClick={() => task.completed ? null : props.completeFn(task.id) }>Complete</button>
+        </div>
+    );
+};
+
+export default ListItem;
